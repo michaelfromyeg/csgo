804d80f7b95f2b82cba617e92898fdb2024a9bfb
Michael DeMarco <michaelfromyeg@gmail.com>
2021-11-08 23:22:12 -0800

    fix: destination in push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cp "./config.cfg" "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\cfg"
+cp "./config.cfg" "/mnt/c/Program Files (x86)/Steam/steamapps/common/Counter-Strike Global Offensive/csgo/cfg"
 echo "Pushed config to Steam folder!"
